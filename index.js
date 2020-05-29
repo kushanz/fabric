a7db917e99c49570136dd7168778bdf25abacd57
kushanz <kushanshanuka@yahoo.com>
2020-05-29 15:45:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,17 +2,31 @@
 import './style.css';
 
 var canvas = new fabric.Canvas('mycanvas');
+
 var rect = new fabric.Rect({
             originX: 'center',
             originY: 'center',
-            top : 0,
+            top : 190,
             left : 0,
             width : 60,
             height : 70,
             fill : 'red'
         });
+        rect.setControlsVisibility({
+          mt: false,
+          mb: false,
+          ml: false,
+          mr: false,
+        })
+        
  canvas.centerObject(rect);
         canvas.add(rect);
+        canvas.item(0).set({
+    borderColor: 'red',
+    cornerColor: 'green',
+    cornerSize: 10,
+    transparentCorners: false
+  });
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
