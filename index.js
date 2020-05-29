b41cfdff76b5233dc38fbe8715232b6a7a3c382e
kushanz <kushanshanuka@yahoo.com>
2020-05-29 12:25:54 +0530

    center object

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Import stylesheets
 import './style.css';
 
+var canvas = new fabric.Canvas('mycanvas');
+var rect = new fabric.Rect({
+            originX: 'center',
+            originY: 'center',
+            top : 0,
+            left : 0,
+            width : 60,
+            height : 70,
+            fill : 'red'
+        });
+ canvas.centerObject(rect);
+        canvas.add(rect);
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
