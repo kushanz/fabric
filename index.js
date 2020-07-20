124b8cc1a46a7ac5ff88e40817535502aaa96e03
kushanz <kushanshanuka@yahoo.com>
2020-07-20 20:04:55 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 import {fabric} from 'fabric';
 var canvas = new fabric.Canvas('mycanvas');
-
+// canvas.backgroundColor = 'yellow';
 var rect = new fabric.Rect({
             originX: 'center',
             originY: 'center',
@@ -18,15 +18,42 @@ var rect = new fabric.Rect({
           ml: false,
           mr: false,
         })
-        
+
+// ad doc image
+var imgDoc = document.getElementById('imgdoc');
+var docInstance = new fabric.Image(imgDoc, {
+  left: 0,
+  top: 0,
+  angle: 0,
+  opacity: 0.75,
+  width:450,
+  height:500,
+  hasControls:false,
+  lockMovementX: false,
+  lockMovementY:false
+})
+docInstance.lockMovementX = false
+
  canvas.centerObject(rect);
+        canvas.add(docInstance);
         canvas.add(rect);
-        canvas.item(0).set({
+        canvas.item(1).set({
     borderColor: 'red',
     cornerColor: 'green',
     cornerSize: 10,
     transparentCorners: false
   });
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+  canvas.item(0).selectable = false;
+
+
+// mouse wheel zooming function
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+});
