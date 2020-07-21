89f2060b363686b6630aed4d5b2a20892e29fa46
kushanz <kushanshanuka@yahoo.com>
2020-07-21 12:11:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var rect = new fabric.Rect({
             left : 0,
             width : 60,
             height : 70,
-            fill : 'red'
+            fill : 'yellow'
         });
         rect.setControlsVisibility({
           mt: false,
@@ -46,70 +46,90 @@ docInstance.lockMovementX = false
     transparentCorners: false
   });
   canvas.item(0).selectable = false;
+canvas.bringForward(rect)
+
+
+fabric.Image.fromURL('https://picsum.photos/450/500',//doc image url
+     function(myImg) {
+        var doc = myImg.set({ 
+                left: 0,
+                top: 0 ,
+                width:450, //set canvas eidth
+                height:500, //set canvas height
+                hasControls:false,
+                lockMovementX: false,
+                lockMovementY:false,
+                selectable:false,
+                sendBackwards:true
+                    // other options 
+            });
+        canvas.add(doc);
+        canvas.sendBackwards(doc)
+})
 
 
 // mouse wheel zooming function
-// canvas.on('mouse:wheel', function(opt) {
-//   var delta = opt.e.deltaY;
-//   var zoom = canvas.getZoom();
-//   zoom *= 0.999 ** delta;
-//   if (zoom > 20) zoom = 20;
-//   if (zoom < 0.01) zoom = 0.01;
-//   canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-//   opt.e.preventDefault();
-//   opt.e.stopPropagation();
-// });
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+});
 
 // button event zoom
-canvas.on('object:moving', function(e) {
-  var obj;
-  obj = e.target;
-  obj.setCoords();
-  var boundingRect = obj.getBoundingRect();
-  var zoom = canvas.getZoom();
-  var viewportMatrix = canvas.viewportTransform;
+// canvas.on('object:moving', function(e) {
+//   var obj;
+//   obj = e.target;
+//   obj.setCoords();
+//   var boundingRect = obj.getBoundingRect();
+//   var zoom = canvas.getZoom();
+//   var viewportMatrix = canvas.viewportTransform;
 
-  //there is a bug in fabric that causes bounding rects to not be transformed by viewport matrix
-  //this code should compensate for the bug for now
-  boundingRect.top = (boundingRect.top - viewportMatrix[5]) / zoom;
-  boundingRect.left = (boundingRect.left - viewportMatrix[4]) / zoom;
-  boundingRect.width /= zoom;
-  boundingRect.height /= zoom;
-  // if object is too big ignore
-  if (obj.currentHeight * zoom > obj.canvas.height * zoom || obj.currentWidth * zoom > obj.canvas.width * zoom) {
-    return;
-  }  
-  var canvasHeight = obj.canvas.height / zoom,
-    canvasWidth = obj.canvas.width / zoom,
-    rTop = boundingRect.top + boundingRect.height,
-    rLeft = boundingRect.left + boundingRect.width;
-  // top-left  corner
-  if (rTop < canvasHeight || rLeft < canvasWidth) {
-    obj.top = Math.max(obj.top, canvasHeight - boundingRect.height);
-    obj.left = Math.max(obj.left, canvasWidth - boundingRect.width);
-  }
-  // bot-right corner
-  if (boundingRect.top + boundingRect.height > obj.canvas.height || boundingRect.left + boundingRect.width > obj.canvas.width) {
-    obj.top = Math.min(obj.top, obj.canvas.height - boundingRect.height + obj.top - boundingRect.top);
-    obj.left = Math.min(obj.left, obj.canvas.width - boundingRect.width + obj.left - boundingRect.left);
-  }
-  //canvas.sendToBack(canvas.getObjects()[0])
-
-
+//   //there is a bug in fabric that causes bounding rects to not be transformed by viewport matrix
+//   //this code should compensate for the bug for now
+//   boundingRect.top = (boundingRect.top - viewportMatrix[5]) / zoom;
+//   boundingRect.left = (boundingRect.left - viewportMatrix[4]) / zoom;
+//   boundingRect.width /= zoom;
+//   boundingRect.height /= zoom;
+//   // if object is too big ignore
+//   if (obj.currentHeight * zoom > obj.canvas.height * zoom || obj.currentWidth * zoom > obj.canvas.width * zoom) {
+//     return;
+//   }  
+//   var canvasHeight = obj.canvas.height / zoom,
+//     canvasWidth = obj.canvas.width / zoom,
+//     rTop = boundingRect.top + boundingRect.height,
+//     rLeft = boundingRect.left + boundingRect.width;
+//   // top-left  corner
+//   if (rTop < canvasHeight || rLeft < canvasWidth) {
+//     obj.top = Math.max(obj.top, canvasHeight - boundingRect.height);
+//     obj.left = Math.max(obj.left, canvasWidth - boundingRect.width);
+//   }
+//   // bot-right corner
+//   if (boundingRect.top + boundingRect.height > obj.canvas.height || boundingRect.left + boundingRect.width > obj.canvas.width) {
+//     obj.top = Math.min(obj.top, obj.canvas.height - boundingRect.height + obj.top - boundingRect.top);
+//     obj.left = Math.min(obj.left, obj.canvas.width - boundingRect.width + obj.left - boundingRect.left);
+//   }
+//   //canvas.sendToBack(canvas.getObjects()[0])
 
 
 
-})
 
-$('#zoomin').click(function() {
-  if (canvas.getZoom() < 3) {
-    canvas.setZoom(canvas.getZoom() + 0.1);
-  }
-});
 
-$('#zoomout').click(function() {
-  if (canvas.getZoom() != 1) {
-    canvas.setZoom(canvas.getZoom() - 0.1)
-  };
+// })
 
-});
+// $('#zoomin').click(function() {
+//   if (canvas.getZoom() < 3) {
+//     canvas.setZoom(canvas.getZoom() + 0.1);
+//   }
+// });
+
+// $('#zoomout').click(function() {
+//   if (canvas.getZoom() != 1) {
+//     canvas.setZoom(canvas.getZoom() - 0.1)
+//   };
+
+// });
