bd833ab1818737f8796a5be8b7e16cf1e6c48336
kushanz <kushanshanuka@yahoo.com>
2020-07-23 19:08:32 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import './style.css';
 import {fabric} from 'fabric';
 import $ from "jquery";
 
-var canvas = new fabric.Canvas('mycanvas');
+var canvas = new fabric.Canvas('mycanvas', {
+  preserveObjectStacking:true
+});
 // canvas.backgroundColor = 'yellow';
+var doc;
 var rect = new fabric.Rect({
             originX: 'center',
             originY: 'center',
@@ -12,14 +15,15 @@ var rect = new fabric.Rect({
             left : 0,
             width : 60,
             height : 70,
-            fill : 'yellow'
+            fill : 'yellow',
+            bringForward:true
         });
-        rect.setControlsVisibility({
-          mt: false,
-          mb: false,
-          ml: false,
-          mr: false,
-        })
+        // rect.setControlsVisibility({
+        //   mt: false,
+        //   mb: false,
+        //   ml: false,
+        //   mr: false,
+        // })
 
 // ad doc image
 // var imgDoc = document.getElementById('imgdoc');
@@ -51,7 +55,7 @@ canvas.bringForward(rect)
 
 fabric.Image.fromURL('https://picsum.photos/450/500',//doc image url
      function(myImg) {
-        var doc = myImg.set({ 
+        doc = myImg.set({ 
                 left: 0,
                 top: 0 ,
                 width:450, //set canvas eidth
@@ -59,7 +63,7 @@ fabric.Image.fromURL('https://picsum.photos/450/500',//doc image url
                 hasControls:false,
                 lockMovementX: false,
                 lockMovementY:false,
-                selectable:false,
+                // selectable:false,
                 sendBackwards:true
                     // other options 
             });
@@ -67,6 +71,8 @@ fabric.Image.fromURL('https://picsum.photos/450/500',//doc image url
         canvas.sendBackwards(doc)
 })
 
+canvas.renderAll();
+  
 
 // mouse wheel zooming function
 canvas.on('mouse:wheel', function(opt) {
@@ -91,14 +97,22 @@ canvas.on('mouse:wheel', function(opt) {
 function resetZoom() {
   // canvas.setZoom(1);
   // canvas.setDimensions({
+  //   top:0,left:0,
   //   width: 450,
   //   height: 500
   // });
+  doc.set({
+    top:0,
+    left:0,
+    width:450,
+    height:500
+  })
   canvas.setViewportTransform([1,0,0,1,0,0]); 
-  // canvas.renderAll()
+  canvas.renderAll()
 }
 
 $('#resetzoom').click(function() {
+  canvas.item(0).selectable = false
   resetZoom()
 })
 
@@ -106,7 +120,7 @@ $('#resetzoom').click(function() {
 $('#zoomin').click(function() {
   if (canvas.getZoom() < 3) {
     // canvas.setZoom(canvas.getZoom() + 0.1);
-    console.log(canvas.getZoom())
+    console.log(canvas.width*canvas.getZoom())
     // let initzoom = canvas.getZoom();
     // canvas.zoomToPoint({ x: 'center', y: 'center' },initzoom+1 );
     canvas.zoomToPoint(new fabric.Point(canvas.width / 2, canvas.height / 2), canvas.getZoom() / 0.9);
@@ -115,11 +129,40 @@ $('#zoomin').click(function() {
 
 // zoom out
 $('#zoomout').click(function() {
-  console.log(canvas.getZoom())
+   console.log(canvas.width*canvas.getZoom())
   if (canvas.getZoom() < 1.1) {
+    canvas.item(0).selectable = false
     canvas.setViewportTransform([1,0,0,1,0,0]); 
   } else {
     canvas.zoomToPoint(new fabric.Point(canvas.width / 2, canvas.height / 2), canvas.getZoom() / 1.1);
   };
 
 });
+
+$('#lockdoc').click(function() {
+  canvas.item(0).selectable =  !canvas.item(0).selectable
+  console.log(canvas.item(0).selectable)
+})
+
+// var canvasresize = function() {
+
+// } 
+
+//   canvas.on('object:moving', function (e) {
+//         var obj = e.target;
+//          // if object is too big ignore
+//         if(obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width){
+//             return;
+//         }        
+//         obj.setCoords();        
+//         // top-left  corner
+//         if(obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0){
+//             obj.top = Math.max(obj.top, obj.top-obj.getBoundingRect().top);
+//             obj.left = Math.max(obj.left, obj.left-obj.getBoundingRect().left);
+//         }
+//         // bot-right corner
+//         if(obj.getBoundingRect().top+obj.getBoundingRect().height  > obj.canvas.height || obj.getBoundingRect().left+obj.getBoundingRect().width  > obj.canvas.width){
+//             obj.top = Math.min(obj.top, obj.canvas.height-obj.getBoundingRect().height+obj.top-obj.getBoundingRect().top);
+//             obj.left = Math.min(obj.left, obj.canvas.width-obj.getBoundingRect().width+obj.left-obj.getBoundingRect().left);
+//         }
+// });
